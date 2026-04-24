f632bbcadc243f46a99e4b01650765e22ecca04b
Jhoseph <jhoseph@zerocool.zerocool>
2026-04-24 00:53:13 -0400

    first commit

---
+++ src/main/java/com/example/e_comerce/ProjectFlisol/ProjectFlisolApplication.java
@@ -0,0 +1,13 @@
+package com.example.e_comerce.ProjectFlisol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectFlisolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectFlisolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/e_comerce/ProjectFlisol/ProjectFlisolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.e_comerce.ProjectFlisol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectFlisolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
