7ee64e44229cd15bea20e85be91d661eb8a1c3a6
Jhoseph <jhoseph@zerocool.zerocool>
2026-04-24 01:00:12 -0400

    add respository

---
+++ src/main/java/com/example/e_comerce/ProjectFlisol/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.example.e_comerce.ProjectFlisol.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.e_comerce.ProjectFlisol.model.Product;
+
+public interface ProductRepository  extends JpaRepository<Product, Long>{
+
+}
