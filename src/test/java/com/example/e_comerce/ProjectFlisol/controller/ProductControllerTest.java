92c10c7ab3ddc6b394aec1506b8899901b35f266
Jhoseph <jhoseph@zerocool.zerocool>
2026-04-24 02:14:50 -0400

    add cicd

---
+++ src/test/java/com/example/e_comerce/ProjectFlisol/controller/ProductControllerTest.java
@@ -0,0 +1,23 @@
+package com.example.e_comerce.ProjectFlisol.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ProductControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void testReturnProducts() throws Exception {
+        mockMvc.perform(get("/product"))
+               .andExpect(status().isOk());
+    }
+}
